af5b6449359b8697d4ce573b76273a08cccc92a1
Levi Halvorson <lhalvorson34@gmail.com>
2020-10-22 20:18:59 -0600

    restarted from scratch. Got the load notes to work.

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,7 +1,7 @@
-const el_noteTitle = document.querySelector('#noteTitle');
-const el_noteText = document.querySelector('#noteText');
-const el_saveNoteBtn = document.querySelector('#saveNoteBtn');
-const el_newNoteBtn = document.querySelector('#newNoteBtn');
+const el_noteTitle = document.querySelector("#noteTitle");
+const el_noteText = document.querySelector("#noteText");
+const el_saveNoteBtn = document.querySelector("#saveNoteBtn");
+const el_newNoteBtn = document.querySelector("#newNoteBtn");
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
@@ -9,20 +9,20 @@ let activeNote = {};
 // this is a function to wrap the POST complexity
 // note you must AWAIT this response.
 // alternatively use jQuery $.post()
-function callUrl( url, data={}, method='post' ){
+function callUrl(url, data = {}, method = "post") {
   // post requires header, method + data to be sent
-  const postData = { 
-      headers: { 'Content-Type': 'application/json' },
-      method,
-      body: JSON.stringify( data )
-  }
-  return fetch( url,postData ).then( res=>res.json() )
+  const postData = {
+    headers: { "Content-Type": "application/json" },
+    method,
+    body: JSON.stringify(data),
+  };
+  return fetch(url, postData).then((res) => res.json());
 }
 
 // If there is an activeNote, display it, otherwise render empty inputs
-function updateActiveCard(){
-  // hide 
-  el_saveNoteBtn.classList.add('d-none');
+function updateActiveCard() {
+  // hide
+  el_saveNoteBtn.classList.add("d-none");
 
   if (activeNote.id) {
     el_noteTitle.readOnly = true;
@@ -35,11 +35,11 @@ function updateActiveCard(){
     el_noteTitle.value = "";
     el_noteText.value = "";
   }
-};
+}
 
 // Get the note data from the inputs, save it to the db and update the view
-async function handleNoteSave(event){
-  event.preventDefault()
+async function handleNoteSave(event) {
+  event.preventDefault();
 
   const newNote = {
     title: el_noteTitle.value,
@@ -47,92 +47,99 @@ async function handleNoteSave(event){
   };
 
   // let it save the note before we trigger a re-render
-  console.log( `[handleNoteSave] saving note: `, newNote )
-  const response = await callUrl( '/api/notes', newNote )
-  console.log( ` .. got from server: `, response )
-  if( response.message ) alert( response.message )
+  console.log(`[handleNoteSave] saving note: `, newNote);
+  const response = await callUrl("/api/notes", newNote);
+  console.log(` .. got from server: `, response);
+  if (response.message) alert(response.message);
 
-  loadAndDisplayNotes()
-  activeNote = {}
-  updateActiveCard()
-};
+  loadAndDisplayNotes();
+  activeNote = {};
+  updateActiveCard();
+}
 
-async function handleNoteDelete( event, noteId ){
-  event.preventDefault()
+async function handleNoteDelete(event, noteId) {
+  event.preventDefault();
 
   // prevents the click listener for the list from being called when the button inside of it is clicked
-  if( !confirm('Are you sure you want to delete this?') )
-    return
+  if (!confirm("Are you sure you want to delete this?")) return;
 
   // delete the note
-  const response = await callUrl( `/api/notes/${noteId}`, {}, 'delete' )
-  if( response.message ) alert( response.message )
+  const response = await callUrl(`/api/notes/${noteId}`, {}, "delete");
+  if (response.message) alert(response.message);
 
-  if( activeNote.id === noteId ){
+  if (activeNote.id === noteId) {
     // oops deleting the actively showing card!
-    activeNote = {}
-    updateActiveCard()
+    activeNote = {};
+    updateActiveCard();
   }
-  
+
   // get and display the cards
   loadAndDisplayNotes();
-};
+}
 
 // Sets the activeNote and displays it
-function handleShowNote( event ){
+function handleShowNote(event) {
   event.preventDefault();
   const id = event.currentTarget.id;
-  console.log( `[handleShowNote] two ways: id='${id}' dataset: `, event.currentTarget.dataset )
+  console.log(
+    `[handleShowNote] two ways: id='${id}' dataset: `,
+    event.currentTarget.dataset
+  );
   activeNote = {
     id,
     title: event.currentTarget.dataset.title,
-    text: event.currentTarget.dataset.text
-  }
+    text: event.currentTarget.dataset.text,
+  };
   updateActiveCard();
-};
+}
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
-function handleCreateNewNote( event ){
+function handleCreateNewNote(event) {
   event.preventDefault();
-  console.log( `[handleCreateNewNote]`, event )
+  console.log(`[handleCreateNewNote]`, event);
 
-  activeNote = {}
+  activeNote = {};
   updateActiveCard();
-};
+}
 
 // if anything typed, we enable the save button
-const handleShowSaveBtn = function ( event ) {
-  console.log( `[handleShowSaveBtn]`)
+const handleShowSaveBtn = function (event) {
+  console.log(`[handleShowSaveBtn]`);
   // if it's an already-created note, then can't edit!
-  if( activeNote.id || (el_noteTitle.value.trim()==='' && el_noteText.value.trim()==='') ){
-    el_saveNoteBtn.classList.add('d-none')
+  if (
+    activeNote.id ||
+    (el_noteTitle.value.trim() === "" && el_noteText.value.trim() === "")
+  ) {
+    el_saveNoteBtn.classList.add("d-none");
   } else {
-    console.log( ' ... showing the save button!' )
-    el_saveNoteBtn.classList.remove('d-none')
+    console.log(" ... showing the save button!");
+    el_saveNoteBtn.classList.remove("d-none");
   }
 };
 
 // Gets notes from the db and renders them to the sidebar
-async function loadAndDisplayNotes(){
-  const notes = await fetch( '/api/notes' ).then( r=>r.json() )
+async function loadAndDisplayNotes() {
+  console.log("running loadAndDisplayNotes...");
+  const notes = await fetch("/api/notes").then((r) => r.json());
 
   const el_noteList = document.querySelector("#noteList");
 
   // clear the list
-  el_noteList.innerHTML = '';
+  el_noteList.innerHTML = "";
 
-  if( notes.length===0 ){
-    el_noteList.innerHTML = `<li class='list-group-item'><span>No save Notes</span></li>`
-    return
+  if (notes.length === 0) {
+    el_noteList.innerHTML = `<li class='list-group-item'><span>No save Notes</span></li>`;
+    return;
   }
 
-  notes.forEach( (note) => {
+  notes.forEach((note) => {
+    console.log("Serving HTML note");
     el_noteList.innerHTML += `
       <li onClick="handleShowNote(event)" id='${note.id}' data-title="${note.title}" data-text="${note.text}" class='list-group-item'><span>${note.title}</span>
       <small onClick="handleNoteDelete(event,'${note.id}')" class='badge bg-secondary float-right'><i class='fas fa-trash-alt icon-resize-small'></i></small>
-      `
+      `;
   });
-};
+}
 
 // Gets and renders the initial list of notes
 loadAndDisplayNotes();
--- server.js
+++ server.js
@@ -1,27 +1,60 @@
-const express = require('express');
-const uuid = require('uuid');
+const express = require("express");
+const uuid = require("uuid");
 const app = express();
+const fs = require("fs");
 
 const PORT = 3000; // process.env.PORT ?
 
 // will share any static html files with the browser
-app.use( express.static('public') );
+app.use(express.static("public"));
 // accept incoming POST requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const dbFile = './app/db.json';
+class NewNote {
+  constructor(id, title, text) {
+    this.id = id;
+    this.title = title;
+    this.text = text;
+  }
+}
 
-let noteList = [{id: "0000-0000-0000-0000", title: 'note1', text: 'note1 text'}];
+const dbFile = "./app/db.json";
+let readNoteList = JSON.parse(fs.readFileSync(dbFile, "utf8"));
+let id = 0;
+let noteList = [
+  { id: "0000-0000-0000-0000", title: "note1", text: "note1 text" },
+];
 
 // Endpoints =================================================
+app.get("/api/notes", async function (req, res) {
+  console.log("getting data...", readNoteList[0]);
+  res.send(readNoteList);
+});
 
+app.post("/api/notes", async function (req, res) {
+  // ... code ... ?
+  class NewNote {
+    constructor(id, title, text) {
+      this.id = id;
+      this.title = title;
+      this.text = text;
+    }
+  }
+
+  let newNote = new NewNote(uuid.v4(), req.body.title, req.body.text);
+  console.log("NEWNOTE: ", newNote);
+  noteList.push(newNote);
+  console.log("NOTE ARRAY: ", noteList);
+  fs.writeFileSync(dbFile, JSON.stringify(noteList));
+  id++;
+  res.send({ message: "added note..." });
+});
 // you will need to create 3 endpoints here, and it should work magically :)
 // note: for app.post: newNote.id = uuid.v4() // use a random unique id.
 // ... code away ...
 
-
 // Listener ==================================================
-app.listen(PORT, function() {
-    console.log(`Serving notes on PORT ${PORT}`)
-})
+app.listen(PORT, function () {
+  console.log(`Serving notes on PORT ${PORT}, http://localhost:${PORT}`);
+});
